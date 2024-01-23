373475668ee2c8ea65d8fa01a37c487a1ec456b8
OksanaVovk <oksvovk85@gmail.com>
2024-01-23 19:18:45 +0200

    Update app.js
    cors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
@@ -17,12 +17,30 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: 'http://oksanavovk.github.io',
-  })
-);
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://oksanavovk.github.io',
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+  next();
+});
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: 'http://oksanavovk.github.io',
+//   })
+// );
 // app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 
